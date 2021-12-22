bfc85fbb6ad14a083269377f6c40b223c5d04eb4
ppedziwiatr <peter@redstone.finance>
2021-12-22 23:01:17 +0100

    feat: contract search implemented (WIP)

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,114 +1,140 @@
 <template>
-    <b-navbar class="header d-print-none app-header">
-        <b-nav>
-            <div class="logo-container">
-                <a href="/">
-                    <div class="logo-image-container">
-                        <img
-                            class="logo-image first"
-                            src="/smartweave-logo.svg"
-                        />
-                    </div>
-                </a>
-            </div>
-        </b-nav>
-        <b-nav>
-            <b-nav-item>
-                <a class="fs-lg d-md-none" href="#" @click="toggleSidebarMenu">
-                    <i class="fi flaticon-menu" />
-                </a>
-            </b-nav-item>
-        </b-nav>
-        <b-nav class="w-25 w-md-auto">
-            <b-form class="ml-1" inline>
-                <b-form-group style="margin-bottom: 0">
-                    <b-input-group
-                        v-if="showSearchInputInHeader"
-                        class="input-group-no-border"
-                    >
-                        <template v-slot:prepend>
-                            <b-input-group-text
-                                ><i class="fi flaticon-search-2"
-                            /></b-input-group-text>
-                        </template>
-                        <b-form-input
-                            v-model="searchTerm"
-                            id="search-input"
-                            placeholder="Search..."
-                        />
-                    </b-input-group>
-                    <a href="javascript:window.history.back()" v-else>
-                        <i class="fa flaticon-chevron-back" />
-                    </a>
-                </b-form-group>
-            </b-form>
-        </b-nav>
-        <b-nav class="align-items-center flex-grow-1 justify-content-end">
-            <b-modal id="modal-1" title="Code snippet" size="xl">
-                <CodeExample />
-                <template #modal-footer><div></div></template>
-            </b-modal>
-        </b-nav>
-    </b-navbar>
+  <b-navbar class="header d-print-none app-header">
+    <b-nav>
+      <div class="logo-container">
+        <a href="/">
+          <div class="logo-image-container">
+            <img
+                class="logo-image first"
+                src="/smartweave-logo.svg"
+            />
+          </div>
+        </a>
+      </div>
+    </b-nav>
+    <b-nav>
+      <b-nav-item>
+        <a class="fs-lg d-md-none" href="#" @click="toggleSidebarMenu">
+          <i class="fi flaticon-menu"/>
+        </a>
+      </b-nav-item>
+    </b-nav>
+    <b-nav class="w-25 w-md-auto">
+      <b-form class="ml-1" inline>
+        <b-form-group style="margin-bottom: 0">
+          <b-input-group
+              v-if="showSearchInputInHeader"
+              class="input-group-no-border"
+          >
+            <template v-slot:prepend>
+              <b-input-group-text
+              ><i class="fi flaticon-search-2"
+              /></b-input-group-text>
+            </template>
+
+            <vue-typeahead-bootstrap
+                v-model="query"
+                :ieCloseFix="false"
+                :data="foundContracts"
+                :serializer="item => item.contract_id"
+                @hit="goToContract"
+                @input="lookupContracts"
+                placeholder="Search Contracts"
+            />
+
+          </b-input-group>
+          <a href="javascript:window.history.back()" v-else>
+            <i class="fa flaticon-chevron-back"/>
+          </a>
+        </b-form-group>
+      </b-form>
+    </b-nav>
+  </b-navbar>
 </template>
 
 <script>
+import VueTypeaheadBootstrap from 'vue-typeahead-bootstrap';
 import _ from 'lodash';
-import { mapState, mapActions } from 'vuex';
+import {mapState, mapActions} from 'vuex';
+import {debounce} from "lodash/function";
 
 export default {
-    name: 'Header',
-    data() {
-        return {
-            search: this.$route.query.search,
-        };
-    },
-    computed: {
-        ...mapState('layout', [
-            'sidebarClose',
-            'sidebarStatic',
-            'showSearchInputInHeader',
-        ]),
+  name: 'Header',
+  components: {
+    VueTypeaheadBootstrap
+  },
+  data() {
+    return {
+      query: '',
+      search: this.$route.query.search,
+      selectedContract: null,
+      foundContracts: []
+    };
+  },
+  computed: {
+    ...mapState('layout', [
+      'sidebarClose',
+      'sidebarStatic',
+      'showSearchInputInHeader',
+    ]),
 
-        searchTerm: {
-            get() {
-                return this.$store.state.layout.searchTerm;
+    searchTerm: {
+      get() {
+        return this.$store.state.layout.searchTerm;
+      },
+      set(value) {
+        this.updateSearchTerm(value);
+
+        if (value) {
+          this.$router.push({
+            query: {
+              ...this.$route.query,
+              search: value,
             },
-            set(value) {
-                this.updateSearchTerm(value);
-
-                if (value) {
-                    this.$router.push({
-                        query: {
-                            ...this.$route.query,
-                            search: value,
-                        },
-                    });
-                } else {
-                    const queryWithoutSearchInput = _.omit(this.$route.query, [
-                        'search',
-                    ]);
-                    this.$router.push({ query: queryWithoutSearchInput });
-                }
-            },
-        },
+          });
+        } else {
+          const queryWithoutSearchInput = _.omit(this.$route.query, [
+            'search',
+          ]);
+          this.$router.push({query: queryWithoutSearchInput});
+        }
+      },
     },
+  },
 
-    created() {
-        this.updateSearchTerm(this.$route.query.search);
-    },
+  created() {
+    this.updateSearchTerm(this.$route.query.search);
+  },
 
-    methods: {
-        ...mapActions('layout', [
-            'toggleSidebar',
-            'switchSidebar',
-            'changeSidebarActive',
-            'updateSearchTerm',
-        ]),
-        toggleSidebarMenu() {
-            this.toggleSidebar();
-        },
+  methods: {
+    ...mapActions('layout', [
+      'toggleSidebar',
+      'switchSidebar',
+      'changeSidebarActive',
+      'updateSearchTerm',
+    ]),
+    toggleSidebarMenu() {
+      this.toggleSidebar();
     },
+    goToContract(data) {
+      this.$router.push(`/app/contract/${data.contract_id}`);
+      this.foundContracts = [];
+      this.query = '';
+    },
+    lookupContracts: debounce(function () {
+      if (this.query?.length < 3) {
+        this.foundContracts = [];
+        return;
+      }
+      fetch(`https://gateway.redstone.finance/gateway/contracts/search/${this.query}`)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            this.foundContracts = data;
+          })
+    }, 500)
+  },
 };
 </script>
 
--- src/pages/Redstone/Contracts/Contracts.vue
+++ src/pages/Redstone/Contracts/Contracts.vue
@@ -1,53 +1,53 @@
 <template>
-    <div class="providers-wrapper">
-        <TxList :paging="pages" @page-clicked="onPageClicked">
-            <b-table
-                ref="table"
-                id="assets-table"
-                stacked="md"
-                hover
-                :items="contracts"
-                :fields="fields"
-                @row-clicked="rowClicked"
-            >
-                <template #cell(contractId)="data">
-                    <a
-                        @click="
+  <div class="providers-wrapper">
+    <TxList :paging="pages" @page-clicked="onPageClicked">
+      <b-table
+          ref="table"
+          id="assets-table"
+          stacked="md"
+          hover
+          :items="contracts"
+          :fields="fields"
+          @row-clicked="rowClicked"
+      >
+        <template #cell(contractId)="data">
+          <a
+              @click="
                             $router.push(
                                 '/app/contract/' + data.item.contractId
                             )
                         "
-                        target="_blank"
-                    >
-                        {{ data.item.contractId | tx }}
-                    </a>
-                </template>
+              target="_blank"
+          >
+            {{ data.item.contractId | tx }}
+          </a>
+        </template>
 
-                <template #cell(total)="data">
-                    {{ data.item.total }}
-                </template>
+        <template #cell(total)="data">
+          {{ data.item.total }}
+        </template>
 
-                <template #cell(confirmed)="data">
-                    {{ data.item.confirmed }}
-                </template>
+        <template #cell(confirmed)="data">
+          {{ data.item.confirmed }}
+        </template>
 
-                <template #cell(orphaned)="data">
-                    {{ data.item.orphaned }}
-                </template>
+        <template #cell(orphaned)="data">
+          {{ data.item.orphaned }}
+        </template>
 
-                <template #cell(lastBlockHeight)="data">
-                    {{ data.item.lastInteractionHeight }}
-                </template>
-            </b-table>
-            <div v-if="!contracts">
-                <div
-                    v-for="n in 3"
-                    :key="n"
-                    class="preloader text-preloader manifest-preloader"
-                ></div>
-            </div>
-        </TxList>
-    </div>
+        <template #cell(lastBlockHeight)="data">
+          {{ data.item.lastInteractionHeight }}
+        </template>
+      </b-table>
+      <div v-if="!contracts">
+        <div
+            v-for="n in 3"
+            :key="n"
+            class="preloader text-preloader manifest-preloader"
+        ></div>
+      </div>
+    </TxList>
+  </div>
 </template>
 
 <script>
@@ -56,72 +56,73 @@ import axios from 'axios';
 import TxList from '@/components/TxList/TxList';
 
 export default {
-    name: 'Contracts',
+  name: 'Contracts',
 
-    data() {
-        return {
-            fields: [
-                'contractId',
-                'total',
-                'confirmed',
-                'corrupted',
-                'lastInteractionHeight',
-            ],
-            contracts: null,
-            currentPage: 1,
-            paging: null,
-        };
+  data() {
+    return {
+      fields: [
+        'contractId',
+        'total',
+        'confirmed',
+        'corrupted',
+        'lastInteractionHeight',
+      ],
+      contracts: null,
+      currentPage: 1,
+      paging: null,
+    };
+  },
+
+  mounted() {
+    this.getContracts(this.currentPage);
+  },
+
+  created() {
+  },
+
+  components: {TxList},
+  computed: {
+    pages() {
+      return this.paging ? this.paging : null;
     },
+  },
 
-    mounted() {
-        this.getContracts(this.currentPage);
+  methods: {
+    async onPageClicked(pageNumber) {
+      this.currentPage = pageNumber;
+      this.getContracts(this.currentPage);
     },
+    async getContracts(page) {
+      this.contracts = [];
+      axios
+          .get(
+              `https://d1o5nlqr4okus2.cloudfront.net/gateway/contracts?limit=15&page=${page}`
+          )
 
-    created() {},
-
-    components: { TxList },
-    computed: {
-        pages() {
-            return this.paging ? this.paging : null;
-        },
+          .then(async (fetchedContracts) => {
+            this.paging = fetchedContracts.data.paging;
+            fetchedContracts.data.contracts.forEach((contract) => {
+              this.contracts.push({
+                id: contract.contract,
+                contractId: contract.contract,
+                total: contract.interactions,
+                confirmed: contract.confirmed,
+                corrupted: contract.corrupted,
+                lastInteractionHeight:
+                contract.last_interaction_height,
+              });
+            });
+          });
     },
-
-    methods: {
-        async onPageClicked(pageNumber) {
-            this.currentPage = pageNumber;
-            this.getContracts(this.currentPage);
-        },
-        async getContracts(page) {
-            this.contracts = [];
-            axios
-                .get(
-                    `https://d1o5nlqr4okus2.cloudfront.net/gateway/contracts?limit=15&page=${page}`
-                )
-
-                .then(async (fetchedContracts) => {
-                    this.paging = fetchedContracts.data.paging;
-                    fetchedContracts.data.contracts.forEach((contract) => {
-                        this.contracts.push({
-                            id: contract.contract,
-                            contractId: contract.contract,
-                            total: contract.interactions,
-                            confirmed: contract.confirmed,
-                            corrupted: contract.corrupted,
-                            lastInteractionHeight:
-                                contract.last_interaction_height,
-                        });
-                    });
-                });
-        },
-        rowClicked(record) {
-            this.$set(record, '_showDetails', !record._showDetails);
-        },
-        styleCategory(text, numberOfCategories, index) {
-            return (
-                _.startCase(text) + (index < numberOfCategories - 1 ? ', ' : '')
-            );
-        },
+    rowClicked(record) {
+      this.$set(record, '_showDetails', !record._showDetails);
     },
+    styleCategory(text, numberOfCategories, index) {
+      return (
+          _.startCase(text) + (index < numberOfCategories - 1 ? ', ' : '')
+      );
+    },
+  },
 };
 </script>
 
@@ -129,15 +130,15 @@ export default {
 
 <style lang="scss" scoped>
 tr > th:not(:first-of-type) {
-    text-align: right;
+  text-align: right;
 }
 
 tr > td:not(:first-of-type) {
-    text-align: right;
+  text-align: right;
 }
 
 .flaticon-file-signature {
-    height: 30px;
-    width: 30px;
+  height: 30px;
+  width: 30px;
 }
 </style>
