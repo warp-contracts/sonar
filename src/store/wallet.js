999684aa32ff07c85358c1fb0fa68e281bdfe03a
RafalDziuba <dziuba.rafal98@gmail.com>
2023-02-07 14:56:52 +0100

    Delete comment

--- src/store/wallet.js
+++ src/store/wallet.js
@@ -28,7 +28,6 @@ export default {
   actions: {
     async getTokenBalance({ state, commit }) {
       state.tableLoading = true;
-      //pr fix
       const response = await fetch(`https://contracts.warp.cc/balances?walletAddress=${state.walletAccount}`);
 
       const data = await response.json();
