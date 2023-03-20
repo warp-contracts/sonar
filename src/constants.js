545b27e23945e84fc8677d44e9e878566ed8721a
Asia <ziola.jm@gmail.com>
2023-03-20 15:01:47 +0100

    chore: warp gw url update

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export default {
   arweaveUrl: 'arweave.net',
   redstoneUrl: 'https://api.redstone.finance',
-  gatewayProdUrl: 'https://d1o5nlqr4okus2.cloudfront.net',
+  gatewayProdUrl: 'https://gw.warp.cc/sonar',
   testnetUrl: 'https://d2uxz68ar2en02.cloudfront.net',
   cacheUrl: 'https://d2440r7x1v6779.cloudfront.net',
-  warpGateway: 'https://gateway.warp.cc',
+  warpGateway: 'https://gw.warp.cc/sonar',
 };
